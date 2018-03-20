b52fe49b4709afeb13826c23c3c915b888d29ec8
dina hastari <dinahastari28@gmail.com>
2018-03-20 21:45:43 +0800

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,7 +13,7 @@
         </div>
 </footer>
     <!-- Javascript files-->
-    <script src="<?php echo base_url() ?>'assets/vendor/jquery/jquery.min.js"></script>
+<!--     <script src="<?php echo base_url() ?>'assets/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo base_url() ?>'assets/vendor/popper.js/umd/popper.min.js"></script>
     <script src="<?php echo base_url() ?>'assets/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url() ?>'assets/vendor/jquery.cookie/jquery.cookie.js"> </script>
@@ -24,6 +24,6 @@
     <script src="<?php echo base_url() ?>'assets/js/jquery.parallax-1.1.3.js"></script>
     <script src="<?php echo base_url() ?>'assets/vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
     <script src="<?php echo base_url() ?>'assets/vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
-    <script src="<?php echo base_url() ?>'assets/js/front.js"></script>
+    <script src="<?php echo base_url() ?>'assets/js/front.js"></script> -->
   </body>
 </html>
